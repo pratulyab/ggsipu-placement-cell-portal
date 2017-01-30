73b5a318dfba5cc30c1d49f083358f8e50d9b2a7
TPC <tpc_uscit@gmail.com>
2017-01-30 17:55:49 +0530

    Fix static

---
+++ ipu/static/notification/college.js
@@ -0,0 +1,250 @@
+var Notification = (function() {
+	'use strict'
+    var sphr_create_notification = true;
+	var indices = [];
+	function handleMultipleJquery(){
+		$('a').unbind('click'); // to prevent multiple fires because of reloading of jquery in the rendered template.
+		document.getElementById('create-notifications').removeEventListener('click', generateNewForm);
+        $('#dropdown3').on('click', function(e){e.stopPropagation();});
+		$('.dropdown-button').on('click', function(e){e.preventDefault()});
+		$('nav .brand-logo').on('click', function(e){location.href='';});
+        document.getElementById('create-notifications').addEventListener('click', generateNewForm);
+         // Handles the left panel notification button.
+        $('#notification-anchor').on('click' , function(e){$('#your-notifications').trigger('click');});//Handles the maine notification tab button.
+        $('#dropdown1 a').each(function(i, a){
+			var el = $(a);
+			var target = el.data('links');
+			if (!target)
+				return true;
+			var target_el = $('#tab-bar').find("[href='#" + target + "']");
+			if(!target_el.length)
+				return true;
+			el.on('click', function(e){e.preventDefault();target_el[0].click();});
+		});
+	}
+
+
+    function generateNewForm(e) {
+            e.preventDefault();
+            $('#notify-students-div').html(" ");
+            getForm();
+
+        }
+//Notification Starts.
+//Follow the flow. Up to Down.
+//Gets the first form. Asks to select the streams.
+	function getForm() {
+    	var url = $('#notification').attr('url');
+    	$.ajax({
+    		url : url,
+    		type : 'GET',
+    		async : true,
+    		success : function(data, status, xhr){
+				handleMultipleJquery();
+           		$("#notify-students-div").html(data); 
+                $('#select_streams-form').on('submit' , getStreamsSelected);	
+                
+
+    		}
+    	});
+    }
+
+
+    function getStreamsSelected(e) {
+        e.preventDefault();
+        var stream_list = [];
+        $("#id_stream :selected").each(function (i , selected) {
+                stream_list[i] = $(selected).text().substring(1,4);
+                indices[i] = $(selected).index();
+            });
+        //Blur doesn't work since the dropdown div has a dynamic HTML ID
+        getYearForm(stream_list,indices);        
+    }
+
+//Asks the user to select the year of respective chosen streams.
+    function getYearForm(stream_list, indices) {
+        var url = $('#select_streams-form').attr('action');
+        var token = $('input[name = csrfmiddlewaretoken]').val();
+        $.ajax({
+            url : url,
+            type : 'POST',
+            data : { 'csrfmiddlewaretoken' : token,
+                     'stream_list' : stream_list,
+                     'indices' : indices,
+             },
+            success : function(data , status , xhr){
+				handleMultipleJquery();
+                $('#notify-students-div').html(data);
+                $('#id_if_all').on('change' , function(){
+                    var el = $('#id_students_container').find('select');
+                    if ($(this).is(':checked')) {         
+                        el.attr("disabled" , "");
+                        el.material_select();
+                  } else {
+                        el.removeAttr("disabled");
+                        el.material_select();
+                  }
+                });
+                $('#create_notification-form').on('submit' , getStudentsSelected);
+                $('#id_stream').on('change' , generateNewForm);
+                $('select[id^="id_select_year"]').on('change' , getYearsSelected)
+            }
+        });
+    }
+    function getYearsSelected(e) {
+        e.preventDefault();
+        var stream_to_year = {};
+        
+        
+        var year_list = $("select[name^='select_year_']").each(function() {
+            var id = $(this).attr('id');
+            var year_selected_list = [];
+            $('#' + id + ' :selected').each(function (i, selected) {
+                if($(selected).attr('value') !== "" )
+                    year_selected_list.push($(selected).text().substring(5));
+                
+                id = id.substring(id.length - 3);
+                
+            });       
+            stream_to_year[id] = year_selected_list;
+
+        });
+        submitYearForm(JSON.stringify(stream_to_year));
+    }
+        
+    function submitYearForm(stream_to_year) {
+        var url = $('#create_notification-form').attr('students');
+        var token = $('input[name = csrfmiddlewaretoken]').val();
+        $.ajax({
+            url : url,
+            type : 'POST',
+            data : { 
+                'csrfmiddlewaretoken' : token , 
+                'stream_to_year' : stream_to_year,
+                'indices' : indices,
+            },
+            success : function(data , status , xhr){
+                $('#id_stream').on('change' , generateNewForm);
+                populateStudents(data)
+            }
+        });
+
+    }
+
+    function populateStudents(data){
+        var student_select = $('#id_students_container').find('#id_students');
+        student_select.empty();
+        if(data.length === 0){
+            student_select.append('<option disabled="">No Students</option>')
+        }
+        var i = 1;
+        for(var username of data){
+            var option = '<option value="' + i +'">'+ username +"</option>";
+            student_select.append(option);
+            i++;
+        } 
+        student_select.material_select('destroy');
+        student_select.prop('disabled', false);
+        student_select.material_select();
+        Materialize.updateTextFields();
+    }
+
+    function getStudentsSelected(e) {
+        e.preventDefault();
+        var student_list = [];
+        $('#id_students :selected').each(function (i , selected) {
+                  student_list[i] = $(selected).text();          
+            });
+        submitNotificationForm(student_list);
+    }
+
+//Final function which creates the notification in the database.
+    function submitNotificationForm(student_list) {
+        if(sphr_create_notification){
+            sphr_create_notification = false;
+            var url = $('#create_notification-form').attr('action')
+            var message = $('#id_message').val();
+            var token = $('input[name = csrfmiddlewaretoken]').val();
+            var if_all = $('#id_if_all').prop('checked');
+            if(if_all === true){
+                student_list.length = 0;
+                $('#id_students option').each(function(){
+                    student_list.push($(this).text());
+                });
+            }
+            var if_email = $('#id_if_email').prop('checked');
+            var if_sms = $('#id_if_sms').prop('checked');
+
+            $.ajax({
+                url : url,
+                type : 'POST',
+                data : { 
+                    'csrfmiddlewaretoken' : token , 
+                    'student_list' :student_list,
+                    'message' : message,
+                    'if_sms' : if_sms,
+                    'if_email' : if_email,
+                },
+                success : function(data , status , xhr){
+                    //handleMultipleJquery();
+                    $('#your-notifications').trigger('click');
+                    alert("Successful ! " + data + " students are notified");
+
+                }
+            });
+            sphr_create_notification = true;
+        }
+    }
+//Create Notification Ends.
+
+
+//==============================================================================//
+//============Below are functions to get a college's notifications.=============//
+    function getNotifications() {
+        var url = $('#notification_button').attr('href');
+        $.ajax({
+            url : url,
+            type : 'GET',
+            async : true,
+            success : function(data, status, xhr){
+                populateDiv(data);
+                
+            }
+        });
+        
+    }
+
+    function populateDiv(data) {
+        var raw_html = '';  
+        var icon = '';
+        if(data.length === 0){
+            icon = '<i class="material-icons circle">report_problem</i>' 
+            raw_html = '<li class="collection-item avatar">' + icon + '<span class="title">' + 'None' + '</span>' + '<p>' + 'No notifications found.' + '</p>' + '</li>';
+            $('#your-notifications-div-ul').html(raw_html);
+            return;
+        }                   
+        for(var i = 0 ; i < data.length ; i++){
+            icon = ( (data[i].read === true) ? '<i class="material-icons circle blue">done_all</i>' : '<i class="material-icons circle red">fiber_new</i>');
+            raw_html += '<li class="collection-item avatar">' + icon + 
+                '<span class="title">' + data[i].actor + '</span>' + '<p>' + data[i].message + '</p>' + '</li>';
+        }
+        $('#your-notifications-div-ul').html(raw_html);
+
+
+    }    
+    function viewNotifications(e) {
+        e.preventDefault();
+        getNotifications();
+    }
+
+//==============================End============================//
+
+	return {
+		init: function() {
+            document.getElementById('create-notifications').addEventListener('click', generateNewForm);
+            $('#notification').on('click' , viewNotifications);
+			$('#your-notifications').on('click' , viewNotifications);
+		}
+	}
+
+})();
---
+++ ipu/static/notification/student.js
@@ -0,0 +1,242 @@
+var Notification = (function() {
+	'use strict'
+	function handleMultipleJquery(){
+		$('a').unbind('click');
+		document.getElementById('help').removeEventListener('click', generateNewHelpForm); // to prevent multiple fires because of reloading of jquery in the rendered template.
+		document.getElementById('view-issues').removeEventListener('click', generateIssueList); 
+		$('#dropdown3').on('click', function(e){e.stopPropagation();});
+		$('.dropdown-button').on('click', function(e){e.preventDefault()});
+		$('nav .brand-logo').on('click', function(e){location.href='';});
+		document.getElementById('help').addEventListener('click', generateNewHelpForm);//Re-Binds help button in vertical tab.
+        document.getElementById('view-issues').addEventListener('click' , generateIssueList);
+        $('#notification').on('click' , function(e){$('#your-notifications').trigger('click');});//Handles the maine notification tab button.
+        $('#dropdown1 a').each(function(i, a){
+			var el = $(a);
+			var target = el.data('links');
+			if (!target)
+				return true;
+			var target_el = $('#tab-bar').find("[href='#" + target + "']");
+			if(!target_el.length)
+				return true;
+			el.on('click', function(e){e.preventDefault();target_el[0].click();});
+		});
+	}
+
+//=============================================================================//
+//========Below are functions to handle Help functonality of a Student.========//
+	function generateNewHelpForm(e) {
+            e.preventDefault();
+            $('#help-div').html(" ");
+            getHelpForm();
+
+        }
+
+	function getHelpForm(){
+		var url = $('#notification').attr('url');
+		$.ajax({
+    		url : url,
+    		type : 'GET',
+    		success : function(data, status, xhr){
+           		handleMultipleJquery();
+           		$("#help-div").html(data);
+           		$("#id_subject").attr('length' , '32');
+           		$('#submit_issue-form').on('submit' , submitIssueForm); 
+           			
+    		}
+    	});
+		
+	}
+
+	function submitIssueForm(e){
+		e.preventDefault();
+		var token = $('input[name = csrfmiddlewaretoken]').val();
+		var url = $('#submit_issue-form').attr('action');
+		var issue_type = $('#id_issue_type :selected').val();
+		var subject = $('#id_subject');
+		var message = $('#id_message');
+		fieldEvaluator(subject , 32);
+		fieldEvaluator(message , 2044);
+		if((fieldEvaluator(subject , 32) &&
+		fieldEvaluator(message , 2044))){
+			$.ajax({
+				url : url,
+				type : 'POST',
+				data : { 'csrfmiddlewaretoken' : token,
+						 'issue_type' : issue_type,
+						 'subject' : subject.val(),
+						 'message' : message.val(),
+
+				},
+				success : function(data , status , xhr){
+					$('#view-issues').trigger('click');
+					alert("Successful! Please check again after sometime for the reply.");
+				},
+				error: function(error){
+					alert("Couldn't Submit! Please try again later.");
+				},
+			});
+			 $("#submit_issue-form").unbind('submit');
+		}	
+	}
+
+function fieldEvaluator(input_field , max_length){
+	if( !input_field.val()){
+			if(!input_field.closest('div').has('small').length){
+				input_field.closest('div').append('<small class="help-block error">Required field.</small>');
+				return 0;
+			}
+			else
+				return 0;
+		}
+	if(input_field.val()){
+		if(input_field.val().length>max_length){
+			input_field.closest('div').append('<small class="help-block error">Message is too long.</small>');
+			return 0;
+			}
+		else
+			return 1;
+		}
+
+
+}
+//=====================================End.=======================================//
+
+
+
+
+//=====================View Solution to Issues functionality======================//
+
+	function generateIssueList(e){
+		e.preventDefault();
+		$('#view-issues-div').html('<ul id = "view-issues-div-ul" class="collection"></ul>');
+		var url = $('#view-issues').attr('url');
+		$.ajax({
+			url : url,
+			type : 'GET',
+			success : function(data , status , xhr){
+				populateIssueListDiv(data);
+
+			}
+		});
+
+	}
+
+
+	function populateIssueListDiv(data){
+		var raw_html = '';
+		var icon = '';
+		var reply_anchor = '';
+		var issue_type = '';		
+		if(data.length === 0){
+			icon = '<i class="material-icons circle">report_problem</i>' 
+            raw_html = '<li class="collection-item avatar">' + icon + '<span class="title">' + 'None' + '</span>' + '<p>' + 'No issues reported by you so far.' + '</p>' + '</li>';
+            $('#view-issues-div-ul').html(raw_html);
+            return;
+		}
+		for(var i=0; i<(data.length);i++){
+			if(data[i].is_solved === true) {
+				icon = '<i class="material-icons circle blue">done_all</i>' ;
+				reply_anchor = '<div class="col s3 itemBox"><a href="" identifier='+ data[i].identifier +'><i class="material-icons ">reply</i><div class="caption">View Reply</div></a></div>';
+			}
+			else {
+				icon = '<i class="material-icons circle grey">indeterminate_check_box</i>';
+				reply_anchor = '';
+			}
+			if(data[i].issue_type === 'V')
+				issue_type = 'Verification';
+			else if(data[i].issue_type === 'P')
+				issue_type = 'Placement';
+			else if(data[i].issue_type === 'G')
+				issue_type = 'General';
+			raw_html+= '<li identifier class="collection-item avatar">' + icon +
+				'<div class="row"><div class="col s9"><span class="title">' + "Issue Type :   " + issue_type + "</span>" +
+				'<p>' + "Subject :   " + data[i].subject + '</p></div>' + reply_anchor +'</div></li>'
+		}
+		
+		$('#view-issues-div-ul').html(raw_html);
+		
+		$('#view-issues-div-ul').find('a').on('click' , function(e) {
+			e.preventDefault();
+			var param = '';
+			param = $(this).attr('identifier');
+
+			viewIssueReply(param);
+		});
+
+	}
+	function viewIssueReply(identifier){
+		var url = $('#view-issues-div').attr('url')
+		$.ajax({
+			url : url,
+			type : 'GET',
+			data : {
+				'identifier' : identifier,
+			},
+			success : function(data , status , xhr){
+				handleMultipleJquery();
+				$('#view-issues-div').html(data);
+				$('#ok').on('click' , function(e){$('#your-notifications').trigger('click');});
+			}
+		})
+	}
+
+//=====================================End.=======================================//
+
+
+
+
+//==============================================================================//
+//============Below are functions to get a student's notifications.=============//
+	function getNotifications() {
+        var url = $('#notification_button').attr('href');
+        $.ajax({
+            url : url,
+            type : 'GET',
+            async : true,
+            success : function(data, status, xhr){
+                populateNotificationDiv(data);
+                
+            }
+        });
+        
+    }
+
+    function populateNotificationDiv(data) {
+        var raw_html = '';  
+        var icon = '';
+        if(data.length === 0){
+            icon = '<i class="material-icons circle">report_problem</i>' 
+            raw_html = '<li class="collection-item avatar">' + icon + '<span class="title">' + 'None' + '</span>' + '<p>' + 'No notifications found.' + '</p>' + '</li>';
+            $('#your-notifications-div-ul').html(raw_html);
+            return;
+        }                   
+        for(var i = 0 ; i < data.length ; i++){
+            icon = ( (data[i].read === true) ? '<i class="material-icons circle blue">done_all</i>' : '<i class="material-icons circle red">fiber_new</i>');
+            raw_html += '<li class="collection-item avatar">' + icon + 
+                '<span class="title">' + data[i].actor + '</span>' + '<p>' + data[i].message + '</p>' + '</li>';
+        }
+        $('#your-notifications-div-ul').html(raw_html);
+
+
+    }
+
+	function viewNotifications(e) {
+        e.preventDefault();
+        getNotifications();
+    }
+
+//==============================End============================//
+
+
+	return {
+		init: function() {
+			$('#notification').on('click' , viewNotifications);
+			$('#your-notifications').on('click' , viewNotifications);
+			document.getElementById('help').addEventListener('click' , generateNewHelpForm)
+			document.getElementById('view-issues').addEventListener('click' , generateIssueList);
+
+
+		}
+	}
+
+})();
